980e768efe646443c315879c2b088f81543cd757
Chu-Hsuan Lee <joseph.chuhsuanlee@gmail.com>
2018-10-22 21:41:32 +0200

    Add csv reader tool

--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import codecs
+import csv
+
 from sqlalchemy import create_engine
 from sqlalchemy import event
 from sqlalchemy.orm import sessionmaker
@@ -25,3 +28,24 @@ def update_record(src, tar):
     for k in tar.__table__.columns.keys():
         if k not in ['id'] and getattr(src, k) is not None:
             setattr(tar, k, getattr(src, k))
+
+
+def unicode_csv_dictreader(path, *args, **kwargs):
+    """Create a csv dict reader that copes with encoding correctly."""
+    # utf-8-sig strips off a BOM if it's present
+    stream = codecs.open(path, encoding='utf-8-sig')
+    return UnicodeCSVDictReader(stream, *args, **kwargs)
+
+
+class UnicodeCSVDictReader(csv.DictReader):
+    def __init__(self, unicode_csvfile, *args, **kwargs):
+        decoder = codecs.getdecoder('utf-8')
+        self.decoder = lambda v: decoder(v)[0]
+        utf8_csvfile = codecs.iterencode(unicode_csvfile, encoding='utf-8')
+        # bollicks to csv.DictReader being an oldstyle class
+        csv.DictReader.__init__(self, utf8_csvfile, *args, **kwargs)
+        self.fieldnames = [self.decoder(f) for f in self.fieldnames]
+
+    def next(self):
+        data = csv.DictReader.next(self)
+        return {k: self.decoder(v) for (k, v) in data.iteritems()}
